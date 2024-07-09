a71589eb20e4397c79a76f1c64399816641e4697
juan Manuel <jmgv87191@gmail.com>
2024-07-09 14:46:32 -0700

    repaso

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,1 +1,27 @@
-console.log('asdasd')
+let deck = [];
+const tipos = ['C','D','H','S'];
+const especiales = ['A','J','Q','K']
+
+
+function crearDeck(){
+
+    for (let i = 2; i <=10; i++) {
+        for (let tipo  of tipos) {
+            deck.push( i + tipo)
+            
+        }
+    }
+
+    for (let tipo of tipos) {
+        for ( let esp of especiales  ) {
+            deck.push(esp + tipo)
+        }
+    }
+
+    console.log(deck)
+    deck = _.shuffle(deck)
+    console.log(deck)
+
+}
+
+crearDeck();
